153e6629bab28c507036ad8a11c4230b645b4245
Joshua Huber <huber@paradoxical.net>
2018-12-03 13:37:18 -0500

    day3 tuning ^_^

--- src/day3.rs
+++ src/day3.rs
@@ -1,20 +1,20 @@
 use failure::Error;
-use std::collections::{HashMap, HashSet};
+use itertools::Itertools;
 use std::str::FromStr;
 
 #[derive(Copy, Clone, Debug, PartialEq, Eq, Hash)]
 pub struct Point {
-    pub x: u32,
-    pub y: u32,
+    pub x: u16,
+    pub y: u16,
 }
 
 #[derive(Copy, Clone, Debug, PartialEq)]
 pub struct Claim {
-    pub id: u32,
-    pub x: u32,
-    pub y: u32,
-    pub w: u32,
-    pub h: u32,
+    pub id: u16,
+    x: u16,
+    y: u16,
+    w: u16,
+    h: u16,
 }
 
 impl FromStr for Claim {
@@ -27,24 +27,18 @@ impl FromStr for Claim {
         let wh: Vec<&str> = parts2[1].split('x').collect();
 
         Ok(Self {
-            id: parts[0].parse::<u32>()?,
-            x: xy[0].parse::<u32>()?,
-            y: xy[1].parse::<u32>()?,
-            w: wh[0].parse::<u32>()?,
-            h: wh[1].parse::<u32>()?,
+            id: parts[0].parse()?,
+            x: xy[0].parse()?,
+            y: xy[1].parse()?,
+            w: wh[0].parse()?,
+            h: wh[1].parse()?,
         })
     }
 }
 
 impl Claim {
-    pub fn covers(&self) -> Vec<Point> {
-        let mut points = Vec::new();
-        for x in self.x..(self.x + self.w) {
-            for y in self.y..(self.y + self.h) {
-                points.push(Point { x, y });
-            }
-        }
-        points
+    pub fn iter_points(&self) -> impl Iterator<Item = (u16, u16)> {
+        (self.x..(self.x + self.w)).cartesian_product(self.y..(self.y + self.h))
     }
 }
 
@@ -55,36 +49,44 @@ pub fn input_generator(input: &str) -> Vec<Claim> {
 
 #[aoc(day3, part1)]
 pub fn solve_part1(input: &[Claim]) -> usize {
-    let mut claim_map: HashMap<Point, u32> = HashMap::default();
+    let mut claim_map: [[u16; 1000]; 1000] = [[0; 1000]; 1000];
+    let mut tot = 0;
 
     for claim in input {
-        for pt in claim.covers() {
-            (*claim_map.entry(pt).or_insert(0)) += 1;
+        for (x, y) in claim.iter_points() {
+            let entry = &mut claim_map[x as usize][y as usize];
+            *entry += 1;
+
+            if *entry == 2 {
+                tot += 1;
+            }
         }
     }
 
-    claim_map.values().filter(|v| **v > 1).count()
+    tot
 }
 
 #[aoc(day3, part2)]
-pub fn solve_part2(input: &[Claim]) -> u32 {
-    let mut claim_map: HashMap<Point, Vec<&Claim>> = HashMap::default();
-    let mut candidates: HashSet<u32> = HashSet::default();
+pub fn solve_part2(input: &[Claim]) -> u16 {
+    let mut claim_map: [[u16; 1000]; 1000] = [[0; 1000]; 1000];
+    let mut candidates = hashbrown::HashSet::new();
 
     for claim in input {
         candidates.insert(claim.id);
-
-        for pt in claim.covers() {
-            let cvec = claim_map.entry(pt).or_insert(Vec::new());
-            cvec.push(&claim);
-        }
     }
 
-    claim_map.values().filter(|v| v.len() > 1).for_each(|cl| {
-        cl.iter().for_each(|c| {
-            candidates.remove(&c.id);
-        });
-    });
+    for claim in input {
+        for (x, y) in claim.iter_points() {
+            let entry = &mut claim_map[x as usize][y as usize];
+
+            if *entry != 0 {
+                candidates.remove(&claim.id);
+                candidates.remove(entry);
+            } else {
+                *entry = claim.id;
+            }
+        }
+    }
 
     assert_eq!(candidates.len(), 1);
 
